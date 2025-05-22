9438d104b63a16706dedde41396db8a022cf9b20
Lucas Costa <lnatanfrancacosta@gmail.com>
2025-05-22 19:31:13 -0300

    projeto inicializado

---
+++ src/main/java/com/lucas/akobff/AkobffApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.akobff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkobffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkobffApplication.class, args);
+	}
+
+}
